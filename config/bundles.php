17eccd00a4176e607e4af19d7c328075e560c6b6
Darien Hernandez <dh@hellofresh.com>
2019-04-18 02:22:05 +0200

    Use kevinpapst/adminlte-bundle:^2.0 in favor of avanzu/AdminThemeBundle
    Added to composer
    - composer require symfony/security-bundle
    - composer require doctrine/doctrine-fixtures-bundle
    - composer require fzaninotto/faker --dev
    - composer require maker

--- config/bundles.php
+++ config/bundles.php
@@ -9,5 +9,8 @@ return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
-    Avanzu\AdminThemeBundle\AvanzuAdminThemeBundle::class => ['all' => true],
+    KevinPapst\AdminLTEBundle\AdminLTEBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
 ];
