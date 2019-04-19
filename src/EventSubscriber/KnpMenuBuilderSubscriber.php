2f387c5edd5ad085ab4c4c485fb392f59c114875
Darien Hernandez <dh@hellofresh.com>
2019-04-19 16:55:20 +0200

    Add menu builder base on config file.

--- src/EventSubscriber/KnpMenuBuilderSubscriber.php
+++ src/EventSubscriber/KnpMenuBuilderSubscriber.php
@@ -4,10 +4,27 @@ namespace App\EventSubscriber;
 
 use KevinPapst\AdminLTEBundle\Event\KnpMenuEvent;
 use KevinPapst\AdminLTEBundle\Event\ThemeEvents;
+use Knp\Menu\MenuItem;
+use Symfony\Component\Yaml\Yaml;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
+/**
+ * Create a side menu base on the configuration defined in config/menu/menu.yaml
+ *
+ * Example:
+ *
+ * menu:
+ *  OneMenuItem: # item menu name (required)
+ *      label: '' item menu label (required)
+ *      icon: '' item menu icon (optional)
+ *      route: '' item menu route (optional)
+ *      items: # sub items (optional)
+ *
+ * @package App\EventSubscriber
+ */
 class KnpMenuBuilderSubscriber implements EventSubscriberInterface
 {
+    private const CONFIG_MENU = 'menu.yaml';
 
     public static function getSubscribedEvents(): array
     {
@@ -18,29 +35,44 @@ class KnpMenuBuilderSubscriber implements EventSubscriberInterface
 
     public function onSetupMenu(KnpMenuEvent $event)
     {
+        // Loading menu config
+        $menuItems = Yaml::parseFile(\dirname(__DIR__) . '/../config/menu/' . self::CONFIG_MENU);
+
         $menu = $event->getMenu();
 
         $menu->addChild('MainNavigationMenuItem', [
-            'label' => 'MAIN NAVIGATION',
+            'label'        => 'MAIN NAVIGATION',
             'childOptions' => $event->getChildOptions()
         ])->setAttribute('class', 'header');
 
-        $menu->addChild('blogId', [
-            'route' => '',
-            'label' => 'Blog',
-            'childOptions' => $event->getChildOptions()
-        ])->setLabelAttribute('icon', 'fas fa-tachometer-alt');
+        $this->appendChild($event, $menu, $menuItems['menu']);
+    }
 
-        $menu->getChild('blogId')->addChild('ChildOneItemId', [
-            'route' => '',
-            'label' => 'ChildOneDisplayName',
-            'childOptions' => $event->getChildOptions()
-        ])->setLabelAttribute('icon', 'fas fa-rss-square');
+    /**
+     * Append the sub item (child) to its parent.
+     *
+     * @param KnpMenuEvent $event
+     * @param MenuItem $parent
+     * @param array $child
+     *
+     * @return MenuItem
+     */
+    private function appendChild(KnpMenuEvent $event, MenuItem $parent, array $child): MenuItem
+    {
+        foreach ($child as $key => $item) {
+            /** @var MenuItem $menuItem */
+            $menuItem = $parent->addChild($key, [
+                    'label'        => isset($item['label']) ? $item['label'] : '',
+                    'route'        => isset($item['route']) ? $item['route'] : '',
+                    'childOptions' => $event->getChildOptions(),
+                ]
+            )->setLabelAttribute('icon', isset($item['icon']) ? $item['icon'] : '');
 
-        $menu->getChild('blogId')->addChild('ChildTwoItemId', [
-            'route' => '',
-            'label' => 'ChildTwoDisplayName',
-            'childOptions' => $event->getChildOptions()
-        ]);
+            if (isset($item['items']) && !empty($item['items'])) {
+                $this->appendChild($event, $menuItem, $item['items']);
+            }
+        }
+
+        return $parent;
     }
 }
