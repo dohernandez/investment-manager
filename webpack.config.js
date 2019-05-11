54b895f7b6774dec880ec7793e5f7c4a755383b6
Darien Hernandez <dh@hellofresh.com>
2019-05-11 16:47:51 +0200

    Install transpile babel-loader for webpack
    -  yarn add babel-loader @babel/core @babel/preset-env webpack --dev
    - Add babel.config.js
    - Add module babel-loader to webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,21 @@ module.exports = {
         path: path.resolve(__dirname, 'public', 'build'),
         filename: "[name].js",
     },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                        cacheDirectory: true,
+                    }
+                }
+            }
+        ]
+    },
     plugins: [
         new webpack.ProvidePlugin({
             $: 'jquery',
