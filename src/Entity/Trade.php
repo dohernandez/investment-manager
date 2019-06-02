af2edc98d361bcd31b4b651ba5158f6504c76b56
Darien Hernandez <dh@hellofresh.com>
2019-06-02 23:33:46 +0200

    Add missing import.

--- src/Entity/Trade.php
+++ src/Entity/Trade.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use Gedmo\Timestampable\Traits\TimestampableEntity;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\TradeRepository")
