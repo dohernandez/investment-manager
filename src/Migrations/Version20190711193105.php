eef417f8e20d60b7b5dd634b138d5be084ff7f0b
Darien Hernandez <dh@hellofresh.com>
2019-07-11 21:42:46 +0200

    Create exchange entity.

---
+++ src/Migrations/Version20190711193105.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20190711193105 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE exchange ADD paar_currency VARCHAR(15) DEFAULT NULL, CHANGE from_currency from_currency JSON NOT NULL COMMENT \'(DC2Type:json)\', CHANGE to_currency to_currency JSON NOT NULL COMMENT \'(DC2Type:json)\'');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE exchange DROP paar_currency, CHANGE from_currency from_currency JSON NOT NULL COMMENT \'(DC2Type:json)\', CHANGE to_currency to_currency JSON NOT NULL COMMENT \'(DC2Type:json)\'');
+    }
+}
---
+++ src/Repository/ExchangeRepository.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Exchange;
+use App\VO\Currency;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Symfony\Bridge\Doctrine\RegistryInterface;
+
+/**
+ * @method Exchange|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Exchange|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Exchange[]    findAll()
+ * @method Exchange[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class ExchangeRepository extends ServiceEntityRepository
+{
+    public function __construct(RegistryInterface $registry)
+    {
+        parent::__construct($registry, Exchange::class);
+    }
+
+    // /**
+    //  * @return Exchange[] Returns an array of Exchange objects
+    //  */
+    /*
+    public function findByExampleField($value)
+    {
+        return $this->createQueryBuilder('e')
+            ->andWhere('e.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('e.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    */
+
+    /*
+    public function findOneBySomeField($value): ?Exchange
+    {
+        return $this->createQueryBuilder('e')
+            ->andWhere('e.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+    */
+}
