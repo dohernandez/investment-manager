38b974c1a7c9606e841757e7dcca90581325b821
Darien Hernandez <dh@hellofresh.com>
2019-08-23 00:44:30 +0200

    Add delete button js.

---
+++ assets/js/Components/DeleteRowButton.js
@@ -0,0 +1,38 @@
+'use strict';
+
+import RowButton from "./RowButton";
+
+import $ from 'jquery';
+
+const eventBus = require('js-event-bus')();
+
+class DeleteRowButton extends RowButton {
+    /**
+     *
+     * @param form {Object}
+     * @param url {function}
+     * @param selector {string}
+     */
+    constructor(form, url, selector) {
+        super(selector, function (e) {
+            e.preventDefault();
+
+            // Setting form data.
+            const $row = $(e.currentTarget).closest('tr');
+
+            const title = $row.data('title');
+            const id = $row.data('id');
+
+            form.delete(url(id), id, title)
+                .then((result) => {
+                    if (result.value) {
+                        eventBus.emit('entity_deleted', null, id);
+                    }
+                });
+
+        });
+    }
+}
+
+
+export default DeleteRowButton;
