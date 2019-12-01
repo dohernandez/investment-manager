ff9e91f643938a251b2eb2aaffdad3d8b5bd7310
Darien Hernandez <dh@hellofresh.com>
2019-12-02 00:44:46 +0100

    Migrate Money to infrastructure layer.

---
+++ src/Infrastructure/Money/Money.php
@@ -0,0 +1,195 @@
+<?php
+
+namespace App\Infrastructure\Money;
+
+use App\Entity\Exchange;
+use LogicException;
+
+final class Money
+{
+    public function __construct(Currency $currency, int $value = 0, int $precision = 2)
+    {
+        $this->currency = $currency;
+        $this->value = $value;
+        $this->precision = $precision;
+    }
+
+    /**
+     * @var Currency
+     */
+    private $currency;
+
+    public function getCurrency(): ?Currency
+    {
+        return $this->currency;
+    }
+
+    /**
+     * @var int
+     */
+    private $value;
+
+    public function getValue(): int
+    {
+        return $this->value;
+    }
+
+    /**
+     * @var int
+     */
+    private $precision;
+
+    public function getPrecision(): int
+    {
+        return $this->precision;
+    }
+
+    public function getPreciseValue(): float
+    {
+        return $this->value / $this->getDivisorBy();
+    }
+
+    public function getDivisorBy(): int
+    {
+        if ($this->getPrecision() === 4) {
+            return 10000;
+        }
+
+        return 100;
+    }
+
+    public static function fromUSDValue(?float $value, int $precision = 2): self
+    {
+        return new static(
+            Currency::usd(),
+            $value,
+            $precision
+        );
+    }
+
+    public static function fromEURValue(?float $value, int $precision = 2): self
+    {
+        return new static(
+            Currency::eur(),
+            $value,
+            $precision
+        );
+    }
+
+    public static function fromCADValue(?float $value, int $precision = 2): self
+    {
+        return new static(
+            Currency::cad(),
+            $value,
+            $precision
+        );
+    }
+
+    public function exchange(Currency $currency, array $exchangeRates, int $precision = 2): self
+    {
+        $value = $this->getValue();
+
+        $exchangePaar =  $currency->getPaarExchangeRate($this->getCurrency());
+
+        $exists = false;
+        /** @var Exchange $rate */
+        foreach ($exchangeRates as $paar => $rate) {
+            if ($paar == $exchangePaar) {
+                $value = $this->getPreciseValue() / $rate * $this->getDivisorBy();
+
+                break;
+            }
+        }
+
+        return new static($currency, $value, $precision);
+    }
+
+    public function increase(?Money $value): self
+    {
+        if (!$value) {
+            return $this;
+        }
+
+        if (!$this->getCurrency()->equals($value->getCurrency())) {
+            throw new LogicException('can not increase money value, currency are different');
+        }
+
+
+        if ($this->getPrecision() == $value->getPrecision()) {
+            $value = $this->getValue() + $value->getValue();
+        } else {
+            $value = ($this->getPreciseValue() + $value->getPreciseValue()) * $this->getDivisorBy();
+        }
+
+        return new static($this->getCurrency(), $value, $this->getPrecision());
+    }
+
+    public function decrease(?Money $value): self
+    {
+        if (!$value) {
+            return $this;
+        }
+
+        if (!$this->getCurrency()->equals($value->getCurrency())) {
+            throw new LogicException('can not decrease money value, currency are different');
+        }
+
+        if ($this->getPrecision() == $value->getPrecision()) {
+            $value = $this->getValue() - $value->getValue();
+        } else {
+            $value = ($this->getPreciseValue() - $value->getPreciseValue()) * $this->getDivisorBy();
+        }
+
+        return new static($this->getCurrency(), $value, $this->getPrecision());
+    }
+
+    public function __toString(): string
+    {
+        switch ($this->getCurrency()->getCurrencyCode()) {
+            case Currency::CURRENCY_CODE_CAD: {
+                $toString = sprintf(
+                    '%.2f %s',
+                    $this->getPreciseValue(),
+                    $this->getCurrency()->getSymbol()
+                );
+
+                break;
+            }
+            default: {
+                $toString = sprintf(
+                    '%s %.2f',
+                    $this->getCurrency()->getSymbol(),
+                    $this->getPreciseValue()
+                );
+            }
+        }
+
+        return $toString;
+    }
+
+    public function multiply(float $value): self
+    {
+        if (!$value) {
+            return $this;
+        }
+
+        return new static(
+            $this->getCurrency(),
+            $this->getValue() * $value,
+            $this->getPrecision()
+        );
+    }
+
+    public function divide(float $value): self
+    {
+        if (!$value) {
+            return $this;
+        }
+
+        return new static(
+            $this->getCurrency(),
+            $this->getValue() / $value,
+            $this->getPrecision()
+        );
+    }
+}
---
+++ tests/Infrastructure/Money/MoneyTest.php
@@ -0,0 +1,151 @@
+<?php
+
+namespace App\Tests\Infrastructure\Money;
+
+use App\Infrastructure\Money\Currency;
+use App\Infrastructure\Money\Money;
+use LogicException;
+use PHPUnit\Framework\TestCase;
+
+class MoneyTest extends TestCase
+{
+    /**
+     * @dataProvider usdValueDataProvided
+     */
+    public function testFromValue(int $value, int $precision, float $preciseValue, string $currency, Money $money)
+    {
+        $divisionBy = (int)str_pad(1, $precision + 1, '0', STR_PAD_RIGHT);
+
+        self::assertEquals($value, $money->getValue());
+        self::assertEquals($precision, $money->getPrecision());
+        self::assertEquals($divisionBy, $money->getDivisorBy());
+        self::assertEquals($preciseValue, $money->getPreciseValue());
+        self::assertEquals($currency, $money->getCurrency()->getCurrencyCode());
+    }
+
+    public function usdValueDataProvided()
+    {
+        return [
+            [
+                $value = 1500,
+                $precision = 2,
+                $preciseValue = 15,
+                $currency = Currency::CURRENCY_CODE_USD,
+                Money::fromUSDValue($value),
+
+            ],
+            [
+                $value = 1500,
+                $precision = 4,
+                $preciseValue = 0.15,
+                $currency = Currency::CURRENCY_CODE_EUR,
+                Money::fromEURValue($value, $precision),
+            ],
+            [
+                $value = 1500,
+                $precision = 2,
+                $preciseValue = 15,
+                $currency = Currency::CURRENCY_CODE_CAD,
+                Money::fromCADValue($value, $precision),
+            ],
+        ];
+    }
+
+    public function testDivide()
+    {
+        $money = Money::fromEURValue(1500)->divide(2);
+
+        $this->assertEquals(750, $money->getValue());
+        $this->assertEquals(Currency::CURRENCY_CODE_EUR, $money->getCurrency()->getCurrencyCode());
+    }
+
+    public function testExchange()
+    {
+        $exchangePaar =  Currency::eur()->getPaarExchangeRate(Currency::usd());
+        $exchanges = [
+            $exchangePaar => 1.100,
+        ];
+
+        $money = Money::fromUSDValue(5500)->exchange(Currency::eur(), $exchanges);
+
+        $this->assertEquals(4999, $money->getValue());
+    }
+
+    public function testMultiply()
+    {
+        $money = Money::fromEURValue(1500)->multiply(2);
+
+        $this->assertEquals(3000, $money->getValue());
+        $this->assertEquals(Currency::CURRENCY_CODE_EUR, $money->getCurrency()->getCurrencyCode());
+    }
+
+    public function testDecrease()
+    {
+        $money = Money::fromEURValue(1500)->decrease(Money::fromEURValue(500));
+
+        $this->assertEquals(1000, $money->getValue());
+        $this->assertEquals(Currency::CURRENCY_CODE_EUR, $money->getCurrency()->getCurrencyCode());
+    }
+
+    public function testDecreaseFailDifferentCurrency()
+    {
+        $this->expectException(LogicException::class);
+        $this->expectExceptionMessage('can not decrease money value, currency are different');
+
+        Money::fromEURValue(1500)->decrease(Money::fromUSDValue(500));
+    }
+
+    public function testIncrease()
+    {
+        $money = Money::fromEURValue(1500)->increase(Money::fromEURValue(500));
+
+        $this->assertEquals(2000, $money->getValue());
+        $this->assertEquals(Currency::CURRENCY_CODE_EUR, $money->getCurrency()->getCurrencyCode());
+    }
+
+    public function testIncreaseFailDifferentCurrency()
+    {
+        $this->expectException(LogicException::class);
+        $this->expectExceptionMessage('can not increase money value, currency are different');
+
+        Money::fromEURValue(1500)->increase(Money::fromUSDValue(500));
+    }
+
+    /**
+     * @dataProvider toStringDataProvider
+     */
+    public function testToString(string $toString, Money $money)
+    {
+        $this->assertEquals($toString, (string)$money);
+    }
+
+    public function toStringDataProvider()
+    {
+        return [
+            [
+                '$ 6.00',
+                Money::fromUSDValue(600),
+            ],
+            [
+                '$ -6.00',
+                Money::fromUSDValue(-600),
+            ],
+            [
+                '€ 43.03',
+                Money::fromEURValue(4303),
+            ],
+            [
+                '€ -43.03',
+                Money::fromEURValue(-4303),
+            ],
+            [
+                '16.00 C$',
+                Money::fromCADValue(1600),
+            ],
+            [
+                '-16.00 C$',
+                Money::fromCADValue(-1600),
+            ],
+        ];
+    }
+}
