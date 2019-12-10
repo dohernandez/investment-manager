b866cf10882f38ffbf7642b17dae9adc14b16b0c
Darien Hernandez <dh@hellofresh.com>
2019-12-10 16:46:01 +0100

    Add remove function to the account repository.
    Added remove function to the account repository and force all the datetime to be store as UTC when using doctrine.

--- tests/Infrastructure/Storage/AccountRepositoryTest.php
+++ tests/Infrastructure/Storage/AccountRepositoryTest.php
@@ -8,15 +8,55 @@ use App\Infrastructure\Storage\AccountRepository;
 use App\Infrastructure\UUID;
 use App\Tests\Infrastructure\AppDoctrineKernelTestCase;
 use DateTime;
+use DateTimeZone;
 
 /**
  * @group integration
  * @group infrastructure
  * @group storage
  */
-class AccountRepositoryTest extends AppDoctrineKernelTestCase
+final class AccountRepositoryTest extends AppDoctrineKernelTestCase
 {
+    /**
+     * @see \Doctrine\DBAL\Platforms\AbstractPlatform::getDateTimeFormatString
+     */
+    private const PLATFORM_DATE_TIME_FORMAT = 'Y-m-d H:i:s';
+
     public function testSave()
+    {
+        $account = $this->provideAccount();
+        /** @var AccountRepository $repo */
+        $repo = $this->entityManager
+            ->getRepository(Account::class);
+
+        $repo->save($account);
+
+        // Clearing the em in memory.
+        $this->entityManager->clear();
+        /** @var Account $result */
+        $result = $this->entityManager
+            ->createQuery('SELECT account FROM ' . Account::class . ' account WHERE account.id = :id')
+            ->setParameter('id', $account->getId())
+            ->getSingleResult();
+
+        $this->assertInstanceOf(Account::class, $result);
+        $this->assertEquals($account->getId(), $result->getId());
+        $this->assertEquals($account->getType(), $result->getType());
+        $this->assertEquals($account->getAccountNo(), $result->getAccountNo());
+        $this->assertEquals(
+            $account->getCreatedAt()->format(self::PLATFORM_DATE_TIME_FORMAT),
+            $result->getCreatedAt()->format(self::PLATFORM_DATE_TIME_FORMAT)
+        );
+        $this->assertEquals(
+            $account->getUpdatedAt()->format(self::PLATFORM_DATE_TIME_FORMAT),
+            $result->getUpdatedAt()->format(self::PLATFORM_DATE_TIME_FORMAT)
+        );
+        $this->assertEquals($account->getBalance(), $result->getBalance());
+
+        return $account;
+    }
+
+    private function provideAccount(): Account
     {
         $id = UUID\Generator::generate();
         $name = 'Random Iban 1';
@@ -25,7 +65,7 @@ class AccountRepositoryTest extends AppDoctrineKernelTestCase
         $createdAt = new DateTime();
         $balance = Money::fromEURValue(1000);
 
-        $account = (new Account($id))
+        return (new Account($id))
             ->setName($name)
             ->setType($type)
             ->setAccountNo($accountNo)
@@ -33,6 +73,11 @@ class AccountRepositoryTest extends AppDoctrineKernelTestCase
             ->setUpdatedAt($createdAt)
             ->setBalance($balance)
         ;
+    }
+
+    public function testFind()
+    {
+        $account = $this->provideAccount();
 
         /** @var AccountRepository $repo */
         $repo = $this->entityManager
@@ -40,15 +85,45 @@ class AccountRepositoryTest extends AppDoctrineKernelTestCase
 
         $repo->save($account);
 
+        // Clearing the em in memory.
+        $this->entityManager->clear();
         /** @var Account $result */
-        $result = $this->entityManager->find(Account::class, $id);
+        $result = $repo->find($account->getId());
 
-        $this->assertEquals($name, $result->getName());
-        $this->assertEquals($type, $result->getType());
-        $this->assertEquals($accountNo, $result->getAccountNo());
-        $this->assertEquals($createdAt, $result->getCreatedAt());
-        $this->assertEquals($createdAt, $result->getUpdatedAt());
-        $this->assertEquals($balance, $result->getBalance());
+        $this->assertInstanceOf(Account::class, $result);
+        $this->assertEquals($account->getId(), $result->getId());
+        $this->assertEquals($account->getName(), $result->getName());
+        $this->assertEquals($account->getType(), $result->getType());
+        $this->assertEquals($account->getAccountNo(), $result->getAccountNo());
+        $this->assertEquals(
+            $account->getCreatedAt()->format(self::PLATFORM_DATE_TIME_FORMAT),
+            $result->getCreatedAt()->format(self::PLATFORM_DATE_TIME_FORMAT)
+        );
+        $this->assertEquals(
+            $account->getUpdatedAt()->format(self::PLATFORM_DATE_TIME_FORMAT),
+            $result->getUpdatedAt()->format(self::PLATFORM_DATE_TIME_FORMAT)
+        );
+        $this->assertEquals($account->getBalance(), $result->getBalance());
+    }
+
+    public function testRemove()
+    {
+        $account = $this->provideAccount();
+
+        /** @var AccountRepository $repo */
+        $repo = $this->entityManager
+            ->getRepository(Account::class);
+
+        $repo->save($account);
+
+        $repo->delete($account->getId());
+
+
+        // Clearing the em in memory.
+        $this->entityManager->clear();
+        $result = $this->entityManager->find(Account::class, $account->getId());
+
+        $this->assertNull($result);
     }
 
     /**
