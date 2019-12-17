812701c86e25ffe5fac04f500f02675a28c93e88
Darien Hernandez <dh@hellofresh.com>
2019-12-17 23:59:38 +0100

    Add Transfer Repository interfaces.

---
+++ src/Application/Trasnfer/Repository/TransferRepositoryInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Application\Trasnfer\Repository;
+
+use App\Domain\Transfer\Transfer;
+
+interface TransferRepositoryInterface
+{
+    public function find(string $id): Transfer;
+
+    public function save(Transfer $account);
+}
