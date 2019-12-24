5b12202f82db1e67c1ce2191189fe49e5ae63b50
Darien Hernandez <dh@hellofresh.com>
2019-12-24 01:06:36 +0100

    Migrate get broker endpoints

--- src/Presentation/Controller/Broker/BrokerRESTController.php
+++ src/Presentation/Controller/Broker/BrokerRESTController.php
@@ -3,11 +3,13 @@
 namespace App\Presentation\Controller\Broker;
 
 use App\Application\Broker\Command\RegisterBroker;
+use App\Application\Broker\Repository\ProjectionBrokerRepositoryInterface;
 use App\Domain\Transfer\Transfer;
 use App\Presentation\Controller\InvalidJsonRequestException;
 use App\Presentation\Controller\RESTController;
 use App\Presentation\Form\Broker\CreateBrokerType;
 use Symfony\Component\Form\Form;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Messenger\MessageBusInterface;
@@ -19,6 +21,41 @@ use Symfony\Component\Routing\Annotation\Route;
  */
 final class BrokerRESTController extends RESTController
 {
+    /**
+     * @Route("/", name="broker_list", methods={"GET"}, options={"expose"=true})
+     *
+     * @param ProjectionBrokerRepositoryInterface $repo
+     *
+     * @return JsonResponse
+     */
+    public function all(ProjectionBrokerRepositoryInterface $repo): JsonResponse
+    {
+        return $this->createApiResponse($repo->findAll());
+    }
+
+    /**
+     * @Route("/{id}", name="broker_get", methods={"GET"}, options={"expose"=true})
+     *
+     * @param string $id
+     * @param ProjectionBrokerRepositoryInterface $projectionBrokerRepository
+     *
+     * @return JsonResponse
+     */
+    public function one(string $id, ProjectionBrokerRepositoryInterface $projectionBrokerRepository): JsonResponse
+    {
+        if ($id == '') {
+            return $this->createApiErrorResponse('Broker not found', Response::HTTP_NOT_FOUND);
+        }
+
+        try {
+            $broker = $projectionBrokerRepository->find($id);
+        } catch (\Exception $e) {
+            return $this->createApiErrorResponse($e->getMessage(), Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+
+        return $this->createApiResponse($broker);
+    }
+
     /**
      * @Route("/", name="broker_new", methods={"POST"}, options={"expose"=true})
      *
