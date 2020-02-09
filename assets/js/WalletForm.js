05d9037ba732655a8e5693cdb538769e66ffeebb
Darien Hernandez <dh@hellofresh.com>
2020-02-09 13:18:45 +0100

    Update broker selector with the new structure.

--- assets/js/WalletForm.js
+++ assets/js/WalletForm.js
@@ -1,6 +1,6 @@
 'use strict';
 
-import Form from './Components/Form';
+import SwalForm from './Components/SwalForm';
 import Select2BrokerTemplate from "./Components/Select2BrokerTemplate";
 import $ from 'jquery';
 
@@ -8,14 +8,21 @@ import 'select2';
 
 import './../css/WalletFrom.scss';
 
+const eventBus = require('js-event-bus')();
 /**
- * Form manage how the account form should be build when a crud manager invokes a create or an update action.
+ * Form manage how the wallet form should be build when a crud manager invokes a create or an update action.
  */
-class WalletForm extends Form {
-    constructor(swalFormOptionsText, template = '#js-manager-form-template', selector = '.js-entity-create-from') {
-        super(swalFormOptionsText, template, selector);
+class WalletForm extends SwalForm {
+    constructor(swalOptions, table, template = '#js-table-form-template', selector = '.js-entity-from') {
+        super(swalOptions, template, selector);
 
-        this.select2BrokerTemplate = new Select2BrokerTemplate()
+        this.table = table;
+
+        this.select2BrokerTemplate = new Select2BrokerTemplate();
+
+        eventBus.on("entity_created", this.onCreated.bind(this));
+        eventBus.on("entity_updated", this.onUpdated.bind(this));
+        eventBus.on("entity_deleted", this.onDeleted.bind(this));
     }
 
     /**
@@ -24,7 +31,7 @@ class WalletForm extends Form {
      * @param {Object} data
      * @param $wrapper
      */
-    onBeforeOpen(data, $wrapper) {
+    onBeforeOpenEditView(data, $wrapper) {
         if (data) {
             let $form = $wrapper.find(this.selector);
             for (const property in data) {
@@ -32,7 +39,7 @@ class WalletForm extends Form {
 
                 if (property === 'broker') {
                     let inputData = data[property];
-                    $input.append(new Option(inputData.name + " - " + inputData.account.accountNo, inputData.id));
+                    $input.append(new Option(inputData.title, inputData.id));
 
                     $input.val(inputData.id);
 
@@ -84,6 +91,22 @@ class WalletForm extends Form {
                 templateSelection: this.select2BrokerTemplate.templateSelection
             });
         });
+    }
+
+    onCreated(entity) {
+        this.table.addRecord(entity);
+    }
+
+    onUpdated(entity, $row) {
+        this.table.replaceRecord(entity, entity.id);
+
+        $row.fadeOut('normal', () => {
+            $row.replaceWith(this.table.createRow(entity));
+        });
+    }
+
+    onDeleted(id) {
+        this.table.removeRecord(id);
     }
 }
 
