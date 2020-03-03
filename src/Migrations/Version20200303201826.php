33edba787a06715917e50f2067419be4ef39ae79
Darien Hernandez <dh@hellofresh.com>
2020-03-03 19:26:12 +0100

    Fix migration

--- src/Migrations/Version20200303201826.php
+++ src/Migrations/Version20200303201826.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20200117003516 extends AbstractMigration
+final class Version20200303201826 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -22,7 +22,8 @@ final class Version20200117003516 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE projection_stock DROP value');
+        $this->addSql('ALTER TABLE projection_stock CHANGE description description LONGTEXT DEFAULT NULL');
+        $this->addSql('ALTER TABLE projection_account ADD closed TINYINT(1) DEFAULT \'0\' NOT NULL');
     }
 
     public function down(Schema $schema) : void
@@ -30,6 +31,7 @@ final class Version20200117003516 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE projection_stock ADD value LONGTEXT CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci` COMMENT \'(DC2Type:object)\'');
+        $this->addSql('ALTER TABLE projection_account DROP closed');
+        $this->addSql('ALTER TABLE projection_stock CHANGE description description VARCHAR(255) CHARACTER SET utf8mb4 DEFAULT NULL COLLATE `utf8mb4_unicode_ci`');
     }
 }
