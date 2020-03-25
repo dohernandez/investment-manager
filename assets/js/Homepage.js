849f2a90648f39cbab1345c80ddfe53489e3d2d4
Darien Hernandez <dh@hellofresh.com>
2020-03-25 23:07:59 +0100

    Wrap stock movers/shakers with Homepage

--- assets/js/Homepage.js
+++ assets/js/Homepage.js
@@ -1,6 +1,28 @@
 'use strict';
 
 class Homepage {
-    constructor() {
+    constructor(
+        stockMoversDailyPanelTable,
+        stockShakersDailyPanelTable
+    ) {
+        this.stockMoversDailyPanelTable = stockMoversDailyPanelTable;
+        this.stockShakersDailyPanelTable = stockShakersDailyPanelTable;
+    }
+
+    render() {
+        this.stockMoversDailyPanelTable.render();
+        this.stockShakersDailyPanelTable.render();
+    }
+
+    load() {
+        this.stockMoversDailyPanelTable.loadRows();
+        this.stockShakersDailyPanelTable.loadRows();
+    }
+
+    toggle() {
+        this.stockMoversDailyPanelTable.toggleExpanded();
+        this.stockShakersDailyPanelTable.toggleExpanded();
     }
 }
+
+global.Homepage = Homepage;
