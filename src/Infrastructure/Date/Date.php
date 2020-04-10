8337a70f61cd4aa0b88c799fbc41b645fefe22f5
Darien Hernandez <dh@hellofresh.com>
2020-04-10 13:33:24 +0200

    [31] Start using interface DateTimeInterface for more flexibility

--- src/Infrastructure/Date/Date.php
+++ src/Infrastructure/Date/Date.php
@@ -3,20 +3,21 @@
 namespace App\Infrastructure\Date;
 
 use DateTime;
+use DateTimeInterface;
 
 final class Date
 {
-    public static function getMonth(DateTime $dateTime): int
+    public static function getMonth(DateTimeInterface $dateTime): int
     {
         return $dateTime->format('n');
     }
 
-    public static function getYear(DateTime $dateTime): int
+    public static function getYear(DateTimeInterface $dateTime): int
     {
         return $dateTime->format('Y');
     }
 
-    public static function getDateTimeBeginYear(int $year): DateTime
+    public static function getDateTimeBeginYear(int $year): DateTimeInterface
     {
         return new DateTime($year . '-01-01');
     }
